d68cbdc0fb8c8da9ce7e0be8fb930c0d3da5c961
Erik Jones <ejones99@umd.edu>
2024-06-12 13:35:53 -0400

    Created comparator

---
+++ src/comparator.py
@@ -0,0 +1,32 @@
+import lxml.etree
+import xmltodict
+from deepdiff.diff import DeepDiff
+import lxml
+
+from .api_resource import ApiResource
+
+class Comparator:
+    def __init__(self, xpath_of_resource_in_put_response: str | None = None):
+        self.xpath_of_resource_in_put_response = xpath_of_resource_in_put_response
+
+    def compare(self, api_resources: list[ApiResource]):
+        results: dict = {}
+        for api_resource in api_resources:
+            resource_from_put_request = api_resource.update_response
+            if self.xpath_of_resource_in_put_response:
+                resource_from_put_request = self.pull_xml_element_from_dict(resource_from_put_request, self.xpath_of_resource_in_put_response)
+            diff = DeepDiff(xmltodict.parse(api_resource.xml_from_get_request), xmltodict.parse(resource_from_put_request))
+            results[api_resource.identifier] = diff
+        
+        return results
+    
+    @staticmethod
+    def pull_xml_element_from_dict(xml: bytes, xpath: str) -> bytes:
+        tree: lxml.etree._ElementTree = lxml.etree.fromstring(xml)
+        
+        try:
+            resource = tree.xpath(xpath)[0]
+        except IndexError:
+            raise ValueError(f"Xpath for finding resource in PUT request is not valid: {xpath}")
+
+        return lxml.etree.tostring(resource, pretty_print=True)
---
+++ tests/test_comparator.py
@@ -0,0 +1,47 @@
+import unittest
+
+from src.comparator import Comparator
+from src.api_resource import ApiResource
+
+class TestComparator(unittest.TestCase):
+    def test_comparison_format_correct(self):
+        api_resource = ApiResource("BRILL", "https://url.com")
+        with open("tests/testdata/test_vendor_from_get.xml", "rb") as f:
+            api_resource.xml_from_get_request = f.read()
+        
+        with open ("tests/testdata/test_vendor_from_put.xml", "rb") as f:
+            api_resource.update_response = f.read()
+
+        c = Comparator(xpath_of_resource_in_put_response=None)
+        result = c.compare([api_resource])
+
+        self.assertDictEqual(result, {
+            "BRILL": {
+                'values_changed': {
+                    "root['vendor']['status']['@desc']": {'new_value': 'Inactive', 'old_value': 'Active'}, 
+                    "root['vendor']['status']['#text']": {'new_value': 'INACTIVE', 'old_value': 'ACTIVE'}
+                    }
+                }
+            }
+        )
+    
+    def test_comparator_resource_nested(self):
+        api_resource = ApiResource("BRILL", "https://url.com")
+        with open("tests/testdata/test_vendor_from_get.xml", "rb") as f:
+            api_resource.xml_from_get_request = f.read()
+        
+        with open ("tests/testdata/test_vendor_from_put_nested.xml", "rb") as f:
+            api_resource.update_response = f.read()
+
+        c = Comparator(xpath_of_resource_in_put_response="/results/vendor")
+        result = c.compare([api_resource])
+
+        self.assertDictEqual(result, {
+            "BRILL": {
+                'values_changed': {
+                    "root['vendor']['status']['@desc']": {'new_value': 'Inactive', 'old_value': 'Active'}, 
+                    "root['vendor']['status']['#text']": {'new_value': 'INACTIVE', 'old_value': 'ACTIVE'}
+                    }
+                }
+            }
+        )
