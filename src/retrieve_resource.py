37bc827874e19367cc0b33368cb15906ba7530f5
Erik Jones <ejones99@umd.edu>
2024-07-08 16:19:13 -0400

    Implemented more pretty printing

--- src/retrieve_resource.py
+++ src/retrieve_resource.py
@@ -1,18 +1,10 @@
+from lxml import etree
 import requests
 from .api_resource import ApiResource
 
 def retrieve_resource(api_resource: ApiResource) -> ApiResource:
     response = requests.get(api_resource.api_url, headers={
                             "Accept": "application/xml"})
-    api_resource.xml_from_get_request = response.content
+    api_resource.xml_from_get_request = etree.tostring(etree.fromstring(response.content), pretty_print=True)
 
     return api_resource
-
-
-    # final_update_limit = None
-    # if update_limit and update_limit > 0:
-    #     final_update_limit = update_limit
-
-    # for index, api_resource in enumerate(api_resources):
-    #     if final_update_limit and (index + 1) > final_update_limit:
-    #         break
